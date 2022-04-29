c78f69e6b1dc4c82aa8fea58802c4a851f024a83
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-29 15:28:26 +0200

    Struttura documento

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+
+Milestone 1: Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
+
+Milestone 2: Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+Milestone 3: Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+
+BONUS:
+
+1. modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+
+2. popolare le options della select della milestone 3 dinamicamente.
+
+ */
